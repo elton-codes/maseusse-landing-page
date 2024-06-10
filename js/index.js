d6a4dda555efd29fecb6875acd15176f185dbcba
elton asamoah <eltonasamoah007@gmail.com>
2024-06-10 09:45:41 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+const btn = document.getElementById('btn-btn')
+btn.onclick = function() {
+    alert('more soon')
+}
+
+const vid = document.getElementById('vid-btn')
+vid.onclick = function() {
+    alert('Video Coming Soon')
+}
